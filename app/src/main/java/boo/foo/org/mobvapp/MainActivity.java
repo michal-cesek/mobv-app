6a7ff9c0d066df27b3edf14c12d2af272f6f9d04
michal-cesek <cesek.michal@gmail.com>
2018-11-15 14:16:33 +0100

    Firebase firestore integration - wip

--- app/src/main/java/boo/foo/org/mobvapp/MainActivity.java
+++ app/src/main/java/boo/foo/org/mobvapp/MainActivity.java
@@ -1,15 +1,31 @@
 package boo.foo.org.mobvapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import boo.foo.org.mobvapp.model.Post;
+import boo.foo.org.mobvapp.model.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MY TAG";
+    private FirebaseFirestore db;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +34,62 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        db = FirebaseFirestore.getInstance();
+
+        login();
+
+
+
+    }
+
+    private void addPost(final Post post){
+
+        db.collection(Post.collectionName)
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        post.withId(documentReference.getId());
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    private void login() {
+
+        db.collection(User.collectionName)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                user = document.toObject(User.class)
+                                        .withId(document.getId());
+
+                                Post post = new Post();
+                                post.setUserid(user.getId());
+                                post.setUsername(user.getUsername());
+                                post.setType("image");
+                                post.setImageurl("https://picsum.photos/g/1600/1600?image=123");
+
+                                addPost(post);
+
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
     }
 
     @Override
